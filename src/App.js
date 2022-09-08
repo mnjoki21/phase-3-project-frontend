8c17bb9497beaba9f5e6a9bd59e6162104528891
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-08 23:10:27 +0300

    adding nav abd hoem styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import {Route , Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route , Routes} from 'react-router-dom'
 import About from './pages/About'
 import Customers from './pages/Customers'
 import NavBar from './pages/NavBar'
 import Home from './pages/Home'
 import Purchases from './pages/Purchases'
 import Books from './pages/Books'
+import './Navbar.css'
+import './Home.css'
 
 
 
 function App() {
   return (
     <>
-      <NavBar />
+   
+      <Router >
+        <NavBar />
       <Routes>
         <Route exact path='/' element={ <Home /> } />
         <Route exact path='/purchases' element={ <Purchases /> } />
@@ -20,7 +24,8 @@ function App() {
         <Route exact path='/books' element={ <Books/> } />
         <Route exact path='/about' element={ <About /> } />
         
-      </Routes>
+        </Routes>
+        </Router>
 
     </>
   )
--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>
-      <h3>e-library</h3>
-      <NavLink to='/' >Home</NavLink>
-      <NavLink to='/books'>Books Available</NavLink>
-      <NavLink to='/customers'>Customers</NavLink>
-      <NavLink to='/purchases'>Purchases Made</NavLink>
-      <NavLink to='/about'>About</NavLink>
+    <div className='navbar'>
+      <h3 className='head'>e-library</h3>
+      <NavLink class='nav' to='/' >Home</NavLink>
+      <NavLink class='nav' to='/books'>Books Available</NavLink>
+      <NavLink  class='nav' to='/customers'>Customers</NavLink>
+      <NavLink class='nav' to='/purchases'>Purchases Made</NavLink>
+      <NavLink  class='nav' to='/about'>About</NavLink>
 
     </div>
   )
