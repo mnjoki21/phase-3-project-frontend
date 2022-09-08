83075105a8e9fa3255bcdbe0c23a62a662f7e1dd
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-08 20:37:35 +0300

    add more pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {Route , Routes} from 'react-router-dom'
+import About from './pages/About'
+import Customers from './pages/Customers'
+import NavBar from './pages/NavBar'
+import Home from './pages/Home'
+import Purchases from './pages/Purchases'
+import Books from './pages/Books'
+
+
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={ <Home /> } />
+        <Route exact path='/purchases' element={ <Purchases /> } />
+        <Route exact path='/customers' element={ <Customers /> } />
+        <Route exact path='/books' element={ <Books/> } />
+        <Route exact path='/about' element={ <About /> } />
+        
+      </Routes>
+
+    </>
   )
 }
 
