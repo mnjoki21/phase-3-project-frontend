31a18b6917cc6e56891b08f96e44a9e86b3fcc64
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-09 01:32:24 +0300

    fetching customers

---
+++ public/fetchCustomers.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useFetchCustomers() {
+    const [ customer, setCustomer ] = useState('')
+    const url = 'http://localhost:9292/patients'
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(customer => {
+            setBooks(customer)
+            })
+        .catch(err => console.log(err))
+    }, [])
+    return[customer, setCustomer, url]
+}
+
+export default useFetchCustomers
