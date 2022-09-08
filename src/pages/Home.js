7ac4da21764c0926cbd3422286584a9fdf262637
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-09 01:11:08 +0300

    adding home pag estyling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <section className='home'>
+        <div className='rowcon'>
+          <div className='img'>
+            <img src='https://wallpaper.dog/large/20483336.jpg'/>
+          </div>
+          <div className='content'>
+            <span>e-library</span>
+            <h2>Welcome to the beginning of your reading journey</h2>
+            <p>Are you ready to experience a different world?</p>
+            <button id='enr'><a href='#' id='enrl'>EXPLORE BOOKS </a></button>
+             <button id='ex'><a href='#' id='exp'>BUY NOW</a></button>
+          </div>
+        </div>
+      </section>
+
+    </div>
   )
 }
 
--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { FaBookOpen , FaCartPlus} from 'react-icons/fa';
+import '../Navbar.css'
 
 function NavBar() {
   return (
     <div className='navbar'>
-      <h3 className='head'>e-library</h3>
-      <NavLink class='nav' to='/' >Home</NavLink>
-      <NavLink class='nav' to='/books'>Books Available</NavLink>
-      <NavLink  class='nav' to='/customers'>Customers</NavLink>
-      <NavLink class='nav' to='/purchases'>Purchases Made</NavLink>
-      <NavLink  class='nav' to='/about'>About</NavLink>
+      <h3 className='head'> <FaBookOpen />e-library</h3>
+      <NavLink  to='/' >Home</NavLink>
+      <NavLink to='/books'>Books Available</NavLink>
+      <NavLink  to='/customers'>Customers</NavLink>
+      <NavLink className='my_shop' to='/purchases'> My purchases <FaCartPlus/></NavLink>
+      <NavLink  to='/about'>About</NavLink>
+
+
 
     </div>
   )
