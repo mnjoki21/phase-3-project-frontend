60584286d9b71340415d4eb692850c97b67376a8
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-08 20:16:50 +0300

    add navlink from react router

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+      <h3>e-library</h3>
+      <NavLink ></NavLink>
+      <NavLink></NavLink>
+      <NavLink></NavLink>
+      <NavLink></NavLink>
+      <NavLink></NavLink>
+
+    </div>
   )
 }
 
