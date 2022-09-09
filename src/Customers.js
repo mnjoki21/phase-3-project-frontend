9a75478945c861a51ffaa2406c3cf4be663ef2a8
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-09 10:09:24 +0300

    fetching customers

--- src/Customers.js
+++ src/Customers.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import useFetchCustomers from './FetchCustomers'
 
-function Customers() {
+function Customers({ custom }) {
+  const [ customer, setCustomer ] = useFetchCustomers()
+  const deleteCustomer = () => {
+    fetch(`http://localhost:9292/customers/${custom?.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    .then(res => res.json())
+      .then(customer => console.log(customer))
+      .catch(err => console.log(err))
+    fetch('http://localhost:9292/patients')
+      .then(res => res.json())
+      .then(data => {
+        setCustomer(customer)
+        console.log(customer)
+      })
+    .catch(err => console.loh(err))
+  }
   return (
-    <div>Customers</div>
+    <>
+      { `${custom?.name} ` }  <span style={{cursor: "pointer"}} onClick={deletePat}>🗑</span>
+    </>
   )
 }
-
 export default Customers
